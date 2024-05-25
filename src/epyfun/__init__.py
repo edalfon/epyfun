c23a44a444ce0477b3ec1a9ab6776b5dd1094f51
edalfon <edalfon@gmail.com>
2024-05-25 02:08:32 +0200

    build: expose fns directly

--- src/epyfun/__init__.py
+++ src/epyfun/__init__.py
@@ -1,8 +1,25 @@
 """epyfun."""
 
-from . import fs  # noqa: F401
-from . import plotly  # noqa: F401
-from . import web  # noqa: F401
+from . import fs
+from . import pandas
+from . import plotly
+from . import web
+from .fs import convert_to_utf8
+from .fs import create_dir
+from .fs import get_latest_file
 
 
-__all__ = ["fs", "web", "plotly"]
+__all__ = [
+    "fs",  #
+    "convert_to_utf8",
+    "create_dir",
+    "get_latest_file",
+    "web",  #
+    "download_file",
+    "plotly",  #
+    "splom",
+    "make_hover_template",
+    "pandas",  #
+    "sanitize_column_name",
+    "clean_names",
+]
